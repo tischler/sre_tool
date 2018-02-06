16dc4133e109bb380de4b70a55afd645a797f2ec
Tim Tischler <tischler@purplecoffeecup.com>
2018-02-06 06:42:37 -0800

    Created a command that correctly prints the version

--- lib/sre_tool.rb
+++ lib/sre_tool.rb
@@ -1,5 +1,16 @@
 require "sre_tool/version"
 
+require 'thor'
+
 module SreTool
   # Your code goes here...
+  class Command < Thor
+    desc "Print hello NAME", "say hello to NAME"
+
+    def hello(name)
+      puts "Hello #{name}"
+      puts "My version is #{SreTool::VERSION}"
+    end
+  end
+
 end
